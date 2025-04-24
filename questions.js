a633e8cdc36dfa01c56ae60c058041c8a3626510
Tigerninja <67224884+foceMaster@users.noreply.github.com>
2025-04-24 17:46:29 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,11 @@
+questions = [
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+  }
