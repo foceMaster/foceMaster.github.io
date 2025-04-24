b5d70a218c1ac0c3102d78b769fc61542a41f02b
Tigerninja <67224884+foceMaster@users.noreply.github.com>
2025-04-24 17:48:33 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -8,4 +8,22 @@ questions = [
             "scty": 0
         }
     },
-  }
+    {
+        "question": "It is necessary for the government to intervene in the economy to protect consumers.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The freer the markets, the freer the people.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    }
+];
