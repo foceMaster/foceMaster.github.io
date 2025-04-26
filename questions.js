660c6978912f3ba51c15157f0df5b6d42600dc6d
Folke Cederholm <folke@Rontgenium.local>
2025-04-26 17:47:37 +0200

    Changed values.

--- questions.js
+++ questions.js
@@ -4,43 +4,25 @@ questions = [
         "effect": {
             "econ": 0,
             "dipl": 5,
-            "govt": 10,
-            "scty": 0
+            "govt": 0,
+            "scty": -10
         }
     },
     {
         "question": "Det heter Simons mancave, inte Mimmis pluggrum.",
         "effect": {
             "econ": 0,
-            "dipl": 5,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "Det luktar lite Pannie h&auml;r.",
-        "effect": {
-            "econ": 0,
-            "dipl": 5,
+            "dipl": 0,
             "govt": -5,
-            "scty": 0
-        }
-    },
-    {
-        "question": "Salmonella &auml;r Irynas favoritord.",
-        "effect": {
-            "econ": 0,
-            "dipl": -5,
-            "govt": 5,
-            "scty": 0
+            "scty": 10
         }
     },
     {
         "question": "Simon borde kolla igenom alla datorer igen, f&ouml;r skojs skull.",
         "effect": {
             "econ": 0,
-            "dipl": 10,
-            "govt": -10,
+            "dipl": 5,
+            "govt": 5,
             "scty": 0
         }
     },
@@ -50,61 +32,61 @@ questions = [
             "econ": 0,
             "dipl": 0,
             "govt": 10,
-            "scty": 5
+            "scty": 10
         }
     },
     {
         "question": "Det skulle vara b&auml;st f&ouml;r alla om skolan crowdfundade Ritas f&ouml;rtidspension.",
         "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 10,
-            "scty": -10
-        }
-    },
-    {
-        "question": "Det skulle vara b&auml;st f&ouml;r alla om skolan crowdfundade Irynas f&ouml;rtidspension.",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 5,
-            "scty": -10
-        }
-    },
-    {
-        "question": "Fritidsg&aring;rden borde st&auml;nga.",
-        "effect": {
             "econ": 5,
-            "dipl": -10,
+            "dipl": 10,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Mimmi Johansson &auml;r mer chill &auml;n Mimmi Schill.",
+        "effect": {
+            "econ": -5,
+            "dipl": 10,
             "govt": 0,
             "scty": 0
         }
     },
     {
-        "question": "Marcus borde avvecklas.",
+        "question": "Fritidsg&aring;rden borde st&auml;nga.",
         "effect": {
-            "econ": 5,
-            "dipl": 0,
+            "econ": 0,
+            "dipl": 10,
             "govt": 10,
             "scty": 0
+        }
+    },
+    {
+        "question": "Charlotte &auml;r b&auml;ttre &auml;n Daniel.",
+        "effect": {
+            "econ": -5,
+            "dipl": -10,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Marcus borde avvecklas.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -5
         }
     },
     {
         "question": "PT &auml;r grunden till all v&auml;rldens ondska.",
         "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 10,
-            "scty": -10
-        }
-    },
-    {
-        "question": "Alla l&auml;rare ska vara bev&auml;pnade.",
-        "effect": {
             "econ": 5,
             "dipl": 0,
             "govt": -10,
-            "scty": 0
+            "scty": -5
         }
     },
     {
@@ -119,6 +101,150 @@ questions = [
     {
         "question": "Skolan borde sluta med NP i moderna spr&aring;k.",
         "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Mimmi &auml;r bra men kort.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A-barn &auml;r ett kr&auml;nkade begrepp.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Darkos kropp borde kopplas upp mot Skola24 s&aring; han fysiskt k&auml;nner det varje g&aring;ng en elev f&ouml;r ogiltig fr&aring;nvaro.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Skolan borde p&aring;b&ouml;rja genetiska experiment f&ouml;r att f&aring; fram s&aring; m&aring;nga A-barn som m&ouml;jligt.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "F&ouml;r att folk ska sluta vapea borde kokain bli till&aring;tet inom skolans omr&aring;de.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Grupprummen borde vara &ouml;ppna under rasten.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Niklas gjorde r&auml;tt i att g&aring; till Kungsgrillen under sin lektion.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Alla lektioner borde vara frivilliga.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Elever borde inte &ouml;vervakas alls under studietiden.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Det borde bli gratis att parkera p&aring; skolan.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Simon &auml;r den &ouml;verl&auml;gset b&auml;sta l&auml;raren.",
+        "effect": {
+            "econ": 10,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Ist&auml;llet f&ouml;r IM borde elever utan gymnasiebeh&ouml;righet erbjudas att br&auml;nnas p&aring; b&aring;l.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Mariam borde deporteras till Vita.",
+        "effect": {
+            "econ": 5,
+            "dipl": 10,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Vita har generellt sett mycket b&auml;ttre elever &auml;n Gula.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Det roligaste skolan har att erbjuda &auml;r lektioner.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Matsalen borde bjuda p&aring; efterr&auml;tt varje dag.",
+        "effect": {
             "econ": 0,
             "dipl": 5,
             "govt": 5,
@@ -126,112 +252,58 @@ questions = [
         }
     },
     {
-        "question": "Skolan borde ha en egen brandk&aring;r.",
-        "effect": {
-            "econ": 0,
-            "dipl": 5,
-            "govt": -5,
-            "scty": 0
-        }
-    },
-    {
-        "question": "Alla som f&aring;tt godk&auml;nt g&aring;r till grupprummet.",
-        "effect": {
-            "econ": 10,
-            "dipl": 0,
-            "govt": 0,
-            "scty": -10
-        }
-    },
-    {
-        "question": "Mimmi &auml;r bra men kort.",
-        "effect": {
-            "econ": 0,
-            "dipl": -5,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "Mimmi Johansson &auml;r mer chill &auml;n Mimmi Schill.",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": -5,
-            "scty": 10
-        }
-    },
-    {
-        "question": "F-barn &auml;r ett l&auml;mpligt ord f&ouml;r elever med m&aring;nga F.",
-        "effect": {
-            "econ": 10,
-            "dipl": 0,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "A-barn &auml;r ett kr&auml;nkade begrepp.",
+        "question": "L&auml;rare finns till f&ouml;r att g&ouml;ra elever obekv&auml;ma.",
         "effect": {
             "econ": 0,
             "dipl": 0,
             "govt": -10,
-            "scty": 5
+            "scty": -10
         }
     },
     {
-        "question": "Skolan har f&ouml;r m&aring;nga utrikesf&ouml;dda elever.",
+        "question": "Jag vill sl&aring; Daniel Lindmark i huvudet med en baguette.",
         "effect": {
-            "econ": -10,
-            "dipl": 5,
+            "econ": 0,
+            "dipl": -5,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Spanskan &auml;r &ouml;verl&auml;gsen alla andra spr&aring;k och de andra borde avskaffas, inklusive sven.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
             "govt": 0,
             "scty": 0
         }
     },
     {
-        "question": "Darkos kropp borde kopplas upp mot Skola24 s&aring; han fysiskt k&auml;nner det varje g&aring;ng en elev f&ouml;r ogiltig fr&aring;nvaro.",
+        "question": "Christofer &auml;r en m&auml;nniska.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "F&ouml;r att minska skolkandet borde elever kedjas fast vid sina b&auml;nkar.",
         "effect": {
             "econ": 5,
             "dipl": 0,
             "govt": 10,
-            "scty": 0
+            "scty": 5
         }
     },
     {
-        "question": "Skolan borde p&aring;b&ouml;rja genetiska experiment f&ouml;r att f&aring; fram s&aring; m&aring;nga A-barn som m&ouml;jligt.",
-        "effect": {
-            "econ": 5,
-            "dipl": -5,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "Det finns f&ouml;r mycket gl&auml;djebetyg p&aring; skolan.",
+        "question": "Jag har en bra relation till Darko.",
         "effect": {
             "econ": -5,
-            "dipl": -10,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "F&ouml;r att folk ska sluta vapea borde kokain bli till&aring;tet inom skolans omr&aring;de.",
-        "effect": {
-            "econ": 0,
-            "dipl": 10,
+            "dipl": 5,
             "govt": 10,
-            "scty": 0
+            "scty": 10
         }
     },
-    {
-        "question": "Grupprummen borde vara &ouml;ppna under rasten.",
-        "effect": {
-            "econ": 0,
-            "dipl": 10,
-            "govt": 0,
-            "scty": -10
-        }
-    }
 
 ];
